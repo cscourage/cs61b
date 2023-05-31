d35c194561cf59ad6005d56c25f0a480175fea6e
cscourage <2961358454@qq.com>
2023-05-31 18:58:44 +0800

    revise for style chekcer

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private int N;
     private WeightedQuickUnionUF sites;
-    private WeightedQuickUnionUF sitesAvoidBackWash;
+    private WeightedQuickUnionUF sitesAvoid;
     private int topVirtualSite;
     private int bottomVirtualSite;
     private boolean[][] flags; // true stands for "open", vice verse.
@@ -24,13 +24,13 @@ public class Percolation {
 
         flags = new boolean[N][N];
         sites = new WeightedQuickUnionUF(N * N + 2);
-        sitesAvoidBackWash = new WeightedQuickUnionUF(N * N + 1);
+        sitesAvoid = new WeightedQuickUnionUF(N * N + 1);
         // the reason why I don't put the two below into the same for loop is that
         // it might spoil the array's spatial locality. But I don't verify it.
         // and if you want to speed up, you can also use loop unrolling.
         for (int i = 0; i < N; i += 1) {
             sites.union(topVirtualSite, locationTo1D(0, i));
-            sitesAvoidBackWash.union(topVirtualSite, locationTo1D(0, i));
+            sitesAvoid.union(topVirtualSite, locationTo1D(0, i));
         }
         for (int i = 0; i < N; i += 1) {
             sites.union(bottomVirtualSite, locationTo1D(N - 1, i));
@@ -66,7 +66,7 @@ public class Percolation {
         }
         if (flags[neighborRow][neighborCol]) {
             sites.union(locationTo1D(row, col), locationTo1D(neighborRow, neighborCol));
-            sitesAvoidBackWash.union(locationTo1D(row, col), locationTo1D(neighborRow, neighborCol));
+            sitesAvoid.union(locationTo1D(row, col), locationTo1D(neighborRow, neighborCol));
         }
     }
 
@@ -83,7 +83,7 @@ public class Percolation {
         if (!flags[row][col]) {
             return false;
         }
-        return sitesAvoidBackWash.connected(locationTo1D(row, col), topVirtualSite);
+        return sitesAvoid.connected(locationTo1D(row, col), topVirtualSite);
     }
 
     // number of open sites
@@ -93,6 +93,9 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
+        if (numOpen == 0) {
+            return false;
+        }
         return sites.connected(topVirtualSite, bottomVirtualSite);
     }
 
