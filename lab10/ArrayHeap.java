25be8d7ca5421851c382f1774b31987e01140ad3
cscourage <2961358454@qq.com>
2023-06-07 23:45:03 +0800

    revise the removeMin method, add a if condition for judge whether size equals 0, and if so, we mustn't sink. Maybe this time the autograder will be happy.

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -200,7 +200,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         swap(size, 1);
         contents[size] = null;
         size -= 1;
-        sink(1);
+        if (size != 0) {
+            sink(1);
+        }
         return ret;
     }
 
