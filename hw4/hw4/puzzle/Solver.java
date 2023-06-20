9ba5f90c48b46e43b147c077ca3f164e53b719dd
cscourage <2961358454@qq.com>
2023-06-20 18:26:04 +0800

    finish the skeleton and maybe tomorrow I will conquer it.

---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,25 @@
+package hw4.puzzle;
+
+/** Note that it is an immutable class. */
+
+public class Solver {
+    /** Constructor which solves the puzzle, computing everything necessary
+     *  for moves() and solution() to not have to solve the problem again.
+     *  Solves the puzzle using the A* algorithm. Assumes a solution exists.*/
+    public Solver(WorldState initial) {
+        //TODO
+    }
+
+    /** Returns the minimum number of moves to solve the puzzle starting at
+     *  the initial WorldState.*/
+    public int moves() {
+        //TODO
+        return 0;
+    }
+
+    /** Returns a sequence of WorldStates from the initial WorldState to the solution. */
+    public Iterable<WorldState> solution() {
+        //TODO
+        return null;
+    }
+}
